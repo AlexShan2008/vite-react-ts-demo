4f423b95ea1810ae9a5c942e37c802321147be09
Alex Shan <shantong.sir@gmail.com>
2021-09-02 21:24:27 +0800

    feat: remove unused codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-// import { Demo } from "./demo";
-// import BeautifulDnd from "./react-beautiful-dnd/demo";
-// import { colors } from "@atlaskit/theme";
-import { Example } from "./react-beautiful-dnd/example";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Header } from "@components/shared/header/header.component";
+
+const mode = import.meta.env.DEV;
+
+console.log(mode, "mode");
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,9 +17,6 @@ function App() {
       <Router>
         <div>
           <Header />
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/example">
               <About />
@@ -34,39 +30,6 @@ function App() {
           </Switch>
         </div>
       </Router>
-      {/* <Demo /> */}
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header> */}
     </div>
   );
 }
@@ -78,11 +41,7 @@ function Home() {
 }
 
 function About() {
-  return (
-    <h2>
-      <Example />
-    </h2>
-  );
+  return <h2>About</h2>;
 }
 
 function Users() {
--- src/components/shared/header/header.component.tsx
+++ src/components/shared/header/header.component.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "@assets/images/logo.svg";
 
 export const Header = () => {
   return (
     <nav>
       <ul>
+        <li>
+          <img src={logo} className="App-logo" alt="logo" />
+        </li>
         <li>
           <Link to="/">Home</Link>
         </li>
