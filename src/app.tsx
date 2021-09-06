bb796093a96b01287f9abfe0c628d959ec088bd4
Alex Shan <shantong.sir@gmail.com>
2021-09-06 22:40:42 +0800

    refactor(app.tsx): rename App.tsx to app.tsx
    Set git config core.ignorecase false, so we can change the filename easily.

---
+++ src/app.tsx
@@ -0,0 +1,60 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Header } from "@components/header/header.component";
+import GlobalStyle from "@theme/global-styles";
+import horseUrl from "@assets/images/horse.png";
+import "@styles/app.scss";
+import "@styles/app.less";
+
+const mode = import.meta.env.VITE_APP_ENV;
+
+console.log(mode, "mode");
+
+function App() {
+  return (
+    <div className="app-container">
+      <GlobalStyle />
+
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return (
+    <>
+      <h2>About</h2>
+      <img
+        style={{
+          maxWidth: "100%",
+        }}
+        src={horseUrl}
+        alt="horse"
+      />
+    </>
+  );
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
